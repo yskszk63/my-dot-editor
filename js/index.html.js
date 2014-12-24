b4413ca3eb4503f5840dbce514fc8583b8d44b4d
yskszk63 <yskszk63@gmail.com>
2014-12-25 00:42:36 +0900

    ace -> cm, update libs

--- js/index.html.js
+++ js/index.html.js
@@ -1,42 +1,44 @@
-define(['jquery', 'ace', 'viz', 'canvg', 'bootstrap'], function($, ace, viz, canvg) {
-	var editor = ace.edit('editor');
-	editor.setTheme("ace/theme/eclipse");
-	editor.session.setMode("ace/mode/dot");
-	editor.session.setUseWorker(false);
-	editor.setFontSize($('body').css('font-size'));
-	editor.commands.addCommand({
-		name: 'generate',
-		bindKey: {win: 'Ctrl+F11', mac: 'Ctrl+F11'},
-		exec: function() {
-			$('#generate').click();
-		},
-		readOnly: true
+'use strict';
+define(['jquery', 'cm/lib/codemirror', 'viz', 'canvg/canvg', 'bootstrap', 'cm/mode/javascript/javascript'], function($, codemirror, viz, canvg) {
+    var editor = codemirror.fromTextArea($('#editor')[0], {
+        'mode': 'javascript',
+        'indentWithTabs': true,
+        'lineNumbers': true,
+        'extraKeys': {
+            'Ctrl-Enter': function(cm) {
+                $('#generate').click();
+            }
+        }
+    });
+
+    $('a[href=#]').click(function(event) {
+        event.preventDefault();
+    });
+
+    $('#open').click(function(event) {
+        $('#file-open').click();
+    });
+
+    $('#file-open').change(function(event) {
+        if (this.files.length) {
+            var file = this.files[0];
+            var reader = new FileReader();
+            $(reader).on('load', function(event) {
+                editor.session.setValue(event.target.result);
+            });
+            reader.readAsText(file);
+        }
+    });
+
+    $('#generate').click(function(event) {
+        var dot = editor.getValue();
+        var svg = viz(dot, 'svg');
+        var canvas = $('<canvas>');
+        canvg(canvas[0], svg);
+        var url = canvas[0].toDataURL();
+        $('#save').attr('href', url);
+        $('#image').attr('src', url);
+        $('#output-modal').modal('show');
 	});
 
-	$('#open').click(function(event) {
-		event.preventDefault();
-		$('#file-open').click();
-	});
-	$('#file-open').change(function(event) {
-		if (this.files.length) {
-			var file = this.files[0];
-			var reader = new FileReader();
-			$(reader).on('load', function(event) {
-				editor.session.setValue(event.target.result);
-			});
-			reader.readAsText(file);
-		}
-	});
-	$('#generate').click(function(event) {
-		event.preventDefault();
-
-		var dot = editor.getValue();
-		var svg = viz(dot, 'svg');
-    var canvas = $('<canvas>');
-    canvg(canvas[0], svg);
-    var url = canvas[0].toDataURL();
-		$('#save').attr('href', url);
-    $('#image').attr('src', url);
-		$('#output-modal').modal('show');
-	});
 });
--- js/require-config.js
+++ js/require-config.js
@@ -1,33 +1,28 @@
 var require = {
-	shim: {
-		'bootstrap': {
-			deps: ['jquery']
-		},
-		'ace': {
-			exports: 'ace'
-		},
-		'viz': {
-			exports: 'Viz'
-		},
-    'canvg': {
-      deps: ['rgbcolor', 'StackBlur'],
+  shim: {
+    'bootstrap': {
+      deps: ['jquery']
+    },
+    'viz': {
+      exports: 'Viz'
+    },
+    'canvg/canvg': {
+      deps: ['canvg/rgbcolor', 'canvg/StackBlur'],
       exports: 'canvg'
     },
-    'rgbcolor': {
+    'canvg/rgbcolor': {
       exports: 'RGBColor'
     },
-    'StackBlur': {
+    'canvg/StackBlur': {
       exports: 'stackBlurImage'
     }
-	},
-	paths: {
-		'jquery': 'http://code.jquery.com/jquery-2.1.0.min',
-		'bootstrap': 'http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min',
-		'ace': 'http://cdnjs.cloudflare.com/ajax/libs/ace/1.1.01/ace',
-		//'viz': 'http://mdaines.github.io/viz.js/viz',
-		'viz': 'viz',
-    'rgbcolor': 'http://canvg.googlecode.com/svn/trunk/rgbcolor',
-    'StackBlur': 'http://canvg.googlecode.com/svn/trunk/StackBlur',
-    'canvg': 'http://canvg.googlecode.com/svn/trunk/canvg'
+  },
+  paths: {
+    'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min',
+    'bootstrap': '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min',
+    'cm/lib/codemirror': '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.8.0/codemirror.min',
+    'cm/mode/javascript/javascript': '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.8.0/mode/javascript/javascript.min',
+    'viz': '//rawgit.com/mdaines/viz.js/gh-pages/viz',
+    'canvg': '//rawgit.com/gabelerner/canvg/master'
 	}
 };
