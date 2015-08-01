ef21079aa9cdde2139a9dc0286402a97c8fbba43
yskszk63 <yskszk63@gmail.com>
2015-08-01 08:39:44 +0000

    cm 4.8 to 5.5

--- js/require-config.js
+++ js/require-config.js
@@ -10,8 +10,8 @@ var require = {
   paths: {
     'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
     'bootstrap': '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
-    'cm/lib/codemirror': '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.8.0/codemirror.min',
-    'cm/mode/javascript/javascript': '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.8.0/mode/javascript/javascript.min',
+    'cm/lib/codemirror': '//cdnjs.cloudflare.com/ajax/libs/codemirror/5.5.0/codemirror.min',
+    'cm/mode/javascript/javascript': '//cdnjs.cloudflare.com/ajax/libs/codemirror/5.5.0/mode/javascript/javascript.min',
     'viz': '//rawgit.com/mdaines/viz.js/gh-pages/viz',
     'canvgModule': '//rawgit.com/gabelerner/canvg/master/canvg',
     'rgbcolor': '//rawgit.com/gabelerner/canvg/master/rgbcolor',
