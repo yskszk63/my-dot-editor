05422520893a9b27f55f2e2c60477258bb368dd6
yskszk63 <yskszk63@gmail.com>
2015-10-28 02:27:25 +0900

    for now, not reuse...

--- js/require-config.js
+++ js/require-config.js
@@ -17,7 +17,7 @@ var require = {
   paths: {
     'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
     'bootstrap': '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
-    'viz': '//rawgit.com/mdaines/viz.js/gh-pages/viz',
+    'viz': '../viz',
     'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.min',
   }
 };
